8e9261fbd1e3a4e02d27aeb8f648ce7072fbb017
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-07-03 16:46:46 +0200

    Contact Section 01 Create

---
+++ src/containers/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SectionTitle from '../../components/Headers/h2_v1/h2_v1';
+
+const ContactSection = () => (
+    <section>
+        <SectionTitle>Kontakt</SectionTitle>
+    </section>
+);
+
+export default ContactSection;
